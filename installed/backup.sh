603aac303e4cf8275864897291240c8377bb607a
MMMartt <nielumartin@hotmail.com>
2018-12-26 12:34:18 +0800

    update vscode

--- installed/backup.sh
+++ installed/backup.sh
@@ -5,4 +5,3 @@ brew cask list>brew-cask-list.txt
 pip2 list>pip2-list.txt
 pip3 list>pip3-list.txt
 npm -g list --depth=0 | sed "1d" | sed -E "s/^[\`+-]+\s//" | sed -E "s/@(.*)$//">npm-list.txt
-code --list-extensions | xargs -L 1 echo code --install-extension>vscode-list.txt
--- vscode/backup.sh
+++ vscode/backup.sh
@@ -2,3 +2,4 @@
 cp ~/Library/Application\ Support/Code/User/keybindings.json .
 cp ~/Library/Application\ Support/Code/User/settings.json .
 cp -r ~/Library/Application\ Support/Code/User/snippets .
+code --list-extensions | xargs -L 1 echo code --install-extension>vscode-list.txt
